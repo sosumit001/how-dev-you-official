8787a87617cdf3971115b06958263946b18a2f45
sosumit001 <sosumit001@gmail.com>
2023-11-01 06:03:38 +0530

    [BLOG03](primitive-and-reference-types)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Route,Routes } from 'react-router-dom'
 import BlogLayout from './pages/Layout/BlogLayout'
 import GithubAuth from './pages/docs/blogs/2023/github-auth.mdx'
 import Asynchronous from './pages/docs/blogs/2023/asynchronous-programming.mdx'
+import PrimitiveAndReferenceTypes from './pages/docs/blogs/2023/primitive-reference-datatypes.mdx'
 
 // importing blog
 function App() {
@@ -18,6 +19,8 @@ function App() {
           <Route path='/' element = {<BlogLayout title='How Dev You . Official Blog Page' MDXComp={Blogs} />} />
           <Route path='/blogs/2023/github-auth' element = {<BlogLayout title='Github auth 2.0' MDXComp={GithubAuth} />}/>
           <Route path='/blogs/2023/asynchronous-programming' element = {<BlogLayout title="Asynchronous javascript programming || Callbacks, Promises & Async/Await" MDXComp={Asynchronous} />}/>
+          <Route path='/blogs/2023/primitive-and-reference-types' element = {<BlogLayout title="Javascript Primitive and Reference Data Types" MDXComp={PrimitiveAndReferenceTypes} />}/>
+
         </Routes>
       </div>
     </>
