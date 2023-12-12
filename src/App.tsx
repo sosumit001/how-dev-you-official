e7f71729646ab0d8e7575e9e16c443d5db0077d0
sosumit001 <sosumit001@gmail.com>
2023-12-12 21:29:02 +0530

    ../openai / blog / 12 dec 23

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import BlogLayout from './pages/Layout/BlogLayout'
 import GithubAuth from './pages/docs/blogs/2023/github-auth.mdx'
 import Asynchronous from './pages/docs/blogs/2023/asynchronous-programming.mdx'
 import PrimitiveAndReferenceTypes from './pages/docs/blogs/2023/primitive-reference-datatypes.mdx'
+import OpenaiAssistantsApis from './pages/docs/blogs/2023/openai-assistants-api.mdx'
 
 // importing blog
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path='/blogs/2023/github-auth' element = {<BlogLayout title='Github auth 2.0' MDXComp={GithubAuth} />}/>
           <Route path='/blogs/2023/asynchronous-programming' element = {<BlogLayout title="Asynchronous javascript programming || Callbacks, Promises & Async/Await" MDXComp={Asynchronous} />}/>
           <Route path='/blogs/2023/primitive-and-reference-types' element = {<BlogLayout title="Javascript Primitive and Reference Data Types" MDXComp={PrimitiveAndReferenceTypes} />}/>
+          <Route path='/blogs/2023/openai-assistants-api' element = {<BlogLayout title="OpenAI Assistants APIs :Assistant, Threads and Runs" MDXComp={OpenaiAssistantsApis} />}/>
 
         </Routes>
       </div>
